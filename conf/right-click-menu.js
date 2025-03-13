2ab6f4436f9b1f3f9c279bfa76862a291d73bf47
Atsea111 <127626814+Atsea111@users.noreply.github.com>
2025-03-13 23:06:49 +0800

    Update right-click-menu.js

--- conf/right-click-menu.js
+++ conf/right-click-menu.js
@@ -1,12 +1,12 @@
 /**
- * 网页右键点击后是否弹出自定义菜单
+ * 网页右键点击后是否弹出自定义菜单  false  true
  */
 module.exports = {
   CUSTOM_RIGHT_CLICK_CONTEXT_MENU:
     process.env.NEXT_PUBLIC_CUSTOM_RIGHT_CLICK_CONTEXT_MENU || true, // 自定义右键菜单，覆盖系统菜单
   CUSTOM_RIGHT_CLICK_CONTEXT_MENU_THEME_SWITCH:
     process.env.NEXT_PUBLIC_CUSTOM_RIGHT_CLICK_CONTEXT_MENU_THEME_SWITCH ||
-    true, // 是否显示切换主题
+    false, // 是否显示切换主题
   CUSTOM_RIGHT_CLICK_CONTEXT_MENU_DARK_MODE:
     process.env.NEXT_PUBLIC_CUSTOM_RIGHT_CLICK_CONTEXT_MENU_DARK_MODE || true, // 是否显示深色模式
   CUSTOM_RIGHT_CLICK_CONTEXT_MENU_SHARE_LINK:
